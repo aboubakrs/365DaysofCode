721959f5dc0ec357801478e4a2f016b87e0e6452
aboubakrs <36241209+aboubakrs@users.noreply.github.com>
2018-09-13 01:45:54 +0000

    Day 2 of #365DaysofCode

---
+++ Day2/calcul.py
@@ -0,0 +1,12 @@
+#L'interpréteur de Python peut être utilisé comme Calculatrice de Bureau
+calcul_un = 2+3
+calcul_deux = 3 + 8
+calcul_trois = 22/5
+calcul_quatre = 22//5
+calcul_cinq = 3 - 14
+
+print("Le resultat de 2+3 = " ,calcul_un)
+print("Le resultat de 3+8 = " ,calcul_deux)
+print("Le resultat de 22/5 = " ,calcul_trois)
+print("Le resultat de 22//5 = " ,calcul_quatre)
+print("Le resultat de 3-14 = " ,calcul_cinq)
