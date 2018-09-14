9315f2682eb53a6c2e6a67480f2acb34839962b5
aboubakrs <36241209+aboubakrs@users.noreply.github.com>
2018-09-14 19:15:30 +0000

    Day 3 of #365DaysofCode

---
+++ Day3/Day 3.py
@@ -0,0 +1,22 @@
+""" Exercice : Decrivez le plus clairement et le plus completement possible 
+    ce qui se passe a chacune des trois lignes ci-dessous."""
+largeur = 20 
+#_______Reponse : Crees la variable largeur en l'affectant la variable 20
+hauteur = 5 * 9.3 
+#_______Reponse : Crees la variable hauteur en l'affectant 5 multiplié par 9.3
+print(largeur * hauteur)
+#_______Reponse : Affiche le resultat de l'operation largeur * hauteur
+
+
+""" Exercice : Assignez  les valeurs respectives 3, 5, 7 à trois variables a, 
+    b, c. Effectuez l’opération a-b//c. Interprétez le résultat obtenu."""
+a, b, c = 3, 5, 7
+resultat = a-b//c
+print(resultat)
+''' Reponse : Le resultat obtenu est 3. Celà peut etre interprete par le fait 
+    que la division est prioritaire sur la soustraction. En effet b//c(5//7) 
+    signifie le resultat obtenu en divisant 5 par 7 et c'est égale à 0 donc 
+    b//c = 0. Si on effectue cependant la dernière opération à savoir a-(b//c)
+    c'est à dire 3-(0), le resultat obtenu est 3 doù notre resultat precedant.'''
+#______ It was vekhy Easykh ^_^ !
+
