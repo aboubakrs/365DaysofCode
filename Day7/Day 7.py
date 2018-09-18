08afad130c6b98f56b22e73b1ede686f3ea92d5e
aboubakrs <36241209+aboubakrs@users.noreply.github.com>
2018-09-18 22:57:38 +0000

    Day 7 of #365DaysofCode

---
+++ Day7/Day 7.py
@@ -0,0 +1,36 @@
+#Écrivez un programme qui convertisse en radians un angle fourni au départ en degrés
+#Formule : angle en radian = angle en degré * pi/180
+print("Exercice : Convertir des angles en Degré en Radian")
+pi = 3.14159
+angleDegré = 145
+angleRadian = angleDegré*(pi/180)
+print("La convertion en Radian de ", angleDegré, "° équivaut à ", angleRadian)
+
+
+print()
+print()
+
+
+#Écrivez un programme qui convertisse en degrés un angle fourni au départ en radians. 
+#Formule : angle en degré = angle en radian * 180/pi
+print("Exercice : Convertir des angles en Radian en Degré")
+pi2 = 3.14159
+angleRadian2 = 45.2548
+angleDegré2 = angleRadian2*(180/pi)
+print("La convertion en Degré de ", angleRadian2, "équivaut à ", angleDegré2, "°")
+
+
+print()
+print()
+
+
+#Écrivez un programme qui convertisse en degrés Farahd une température exprimée au départ en degrés Celcius
+#Formule :  TF=TC ×1,8+32
+print("Exercice :  Convertir Température Degré Farahd en Degré Celcius")
+tempDegreCelcius = 32
+tempDegreFarahd = (tempDegreCelcius*1.8)+32
+print("La convertion de", tempDegreCelcius, "°C", "correspond à", tempDegreFarahd, "°F")
+
+
+print()
+print()
