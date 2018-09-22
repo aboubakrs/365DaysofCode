975f87e379262beae33026ae2721778f98f6cb19
aboubakrs <36241209+aboubakrs@users.noreply.github.com>
2018-09-22 00:59:03 +0000

    Day 11 of #365DaysofCode

---
+++ Day11/Day 11 - Exo1.py
@@ -0,0 +1,7 @@
+#Detendons nous avec le module Tokhtue X)
+print("Exercice : La Tokhtuee")
+from turtle import * 
+forward(120) 
+left(90) 
+color('red') 
+forward(80)
---
+++ Day11/Day 11 - Exo2.py
@@ -0,0 +1,11 @@
+#Un truc banal vite fait
+print("Exercice : Un truc banal tres tres banal")
+a = 0     
+if not a:         
+    print('gagné')     
+elif a:         
+    print('perdu')
+
+
+print()
+print()
